38579ecd21fe549529b9ddc321d7cf9f132c7dbd
Omar <abdkaheel9@gmail.com>
2026-06-23 14:15:20 +0300

    add easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Dullany());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      path: 'assets/translation/',
+      supportedLocales: [Locale('ar')],
+      fallbackLocale: Locale('ar'),
+      saveLocale: true,
+      child: const Dullany(),
+    ),
+  );
 }
+
 class Dullany extends StatelessWidget {
   const Dullany({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+    );
   }
 }
